d7543668c2fd4a80c4066dad6072fd036c65cab5
Balwinder413 <balwinderghangas99@gmail.com>
2025-07-18 15:11:11 +0530

    Create basic.js

---
+++ basic/basic.js
@@ -0,0 +1,18 @@
+const accountId=233344
+let accountEmail= "ghhh@gamil.com"
+var accountNumber = 12444
+accountCity ="jaipur"
+
+
+//account id=23445
+accountEmail="hhh@gamil.com"
+accountNumber=22333
+console.log(accountEmail);
+
+console.table([accountEmail,accountId, accountNumber])
+
+
+
+/*
+prefer not use var
+because of issue in block scope and funvtion*/
