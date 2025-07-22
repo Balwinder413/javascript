27befce3892115278661395e5a63626def3e5f85
Balwinder413 <balwinderghangas99@gmail.com>
2025-07-22 19:12:00 +0530

    Create practice02.js

---
+++ Array/practice02.js
@@ -0,0 +1,11 @@
+//given array  prices of items[23,45,67,78,89] all array have offer 10% on them.change the array final price after appying
+
+// for of loop
+// let items =[23, 45, 67,78 ,89]
+// let i=0;
+// for(let val of items){
+//    let offer= val/10;
+//    items[i]=items[i]-offer;
+//    console.log(items[i]);
+// i++;
+// }
