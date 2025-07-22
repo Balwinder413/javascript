01ed577106dd33e3007913e00cc2f5e40aaf116c
Balwinder413 <balwinderghangas99@gmail.com>
2025-07-22 18:55:42 +0530

    Create number-math.js

---
+++ basic/number-math.js
@@ -0,0 +1,23 @@
+// number//
+const balance=200
+console.log(balance);
+console.log(balance.toString());
+console.log(balance. toString().length);
+
+const anotherName=22.2345
+console.log(balance.toPrecision(4));
+const hundreds=10000
+console.log(hundreds.toLocaleString('en-IN'));
+
+
+
+/********** maths*/
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.7));
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.7));// LOWEST VALUE
+console.log(Math.min(3,4,5,6));
+console.log(Math.max(1,3,5,6));
+console.log(Math.random());// 0  and 1 di value dindi a
+console.log((Math.random()*10)+1);
