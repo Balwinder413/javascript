168768e0f481ed310a3990ab58640d15ec2a50c4
Balwinder413 <balwinderghangas99@gmail.com>
2025-07-22 19:04:19 +0530

    Create practice.js

---
+++ condition/practice.js
@@ -0,0 +1,11 @@
+// question related if condition 
+ let mode= dark;
+ let color;
+if(mode === "dark'){
+    color  = "Red";
+ }
+ if(mode === "light"){
+     color = "green";
+ }
+console.log(color);
+  }
