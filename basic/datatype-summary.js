d2d8a4b37194656d6af0bbf44f3bdb983301e8b7
Balwinder413 <balwinderghangas99@gmail.com>
2025-07-22 18:52:30 +0530

    Create datatype-summary.js

---
+++ basic/datatype-summary.js
@@ -0,0 +1,39 @@
+// primitive 
+// 7 types [string,number,boolean,underdefined,null,symbol,bigInt]
+
+
+// number
+let score=22
+
+// boolean
+let loggedIn =true
+ let outsideTemp=null
+
+ // underdefined
+ let email;
+
+ // symbol
+ let id= Symbol('123')
+ let anotherId= Symbol('123')
+ console.log(id == anotherId);
+
+ // bigInt
+ let bigNumber=24565677678879n
+
+
+ // non primitive (refrence)
+// array,objects,functions
+let heroes=["shaktiman","aman"]
+let object 
+{
+    name:"balwinder"
+    age:22
+}
+console.log(typeof heroes)
+// function basic
+ let myFunction=function(){
+    console.log("hello");
+ }
+ console.log(typeof myFunction)
+ 
+ // defination = function(){}
