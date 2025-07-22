fc7ba66723b7b72fa9e6ca8862757ce374ce3346
Balwinder413 <balwinderghangas99@gmail.com>
2025-07-22 19:07:53 +0530

    Create array-part1.js

---
+++ Array/array-part1.js
@@ -0,0 +1,18 @@
+let heroes=["veer ji" ,"ranjeet veer ji","shaktimaan" ,"aladin" ];
+console.log(heroes);
+console.log(heroes[2]);
+
+
+// looping over an aaray
+let superHero=["veer ji" ,"ranjeet veer ji","shaktimaan" ,"aladin" ];
+for(let  i=0; i<superHero.length;i++){
+    console.log(superHero[i]);
+    
+
+}
+
+const myArr=[1, 2, 3, 4, 5]
+const myHeroes=["Ranjeet","veer ji","champo", "pampo"]
+console.log(myArr);
+console.log(myHeroes);
+console.log(myArr.length);
