a490bd3080d735d82d70b0baaaec21992da90c6b
Balwinder413 <balwinderghangas99@gmail.com>
2025-07-22 19:02:39 +0530

    Create if else.js

---
+++ condition/if else.js
@@ -0,0 +1,18 @@
+// if else condition
+let age =42;
+
+if(age>12){
+   console.log(big);
+ }
+ else{
+   console.log( notbig);
+ }
+
+// odd even 
+let num =10;
+
+if(num % 2 === 0){
+    console.log("even");
+ }else{
+  console.log("odd");
+}
