29b07f683e69db6d0fe5411cd2a7191e91797594
Balwinder413 <balwinderghangas99@gmail.com>
2025-07-22 18:54:12 +0530

    Create datatype-part1.js

---
+++ basic/datatype-part1.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all js code to new version 
+// alert (3-3)// we are using node js not browser
+console.log(3-3);
+console.log(3+3);
+
+
+let name="balwider"
+let age=18;
+let isLoggedin="true";
+let state;
+
+ // data types
+ /* number
+ bigint 
+ bollen= true,false
+ string =""
+ null= standalone value
+ underdefined= value not assign
+ symbol=unique
+
+ object 
+ */
+ console.log(typeof null);//null
+ console.log(typeof underdefined);//underdefined
