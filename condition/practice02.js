9831fdaa4611d55a65f6a40779a57c2cf51c6991
Balwinder413 <balwinderghangas99@gmail.com>
2025-07-22 19:05:25 +0530

    Create practice02.js

---
+++ condition/practice02.js
@@ -0,0 +1,9 @@
+// //write a program conditional statement to sout the 3 number
+
+let num1 = 5;
+let num2 = 10;
+let num3 = 15;
+
+if (num1 > 0 && num2 > 0 && num3 > 0) {
+    console.log( "num,="+num1 ,+num2,+num3);
+}
