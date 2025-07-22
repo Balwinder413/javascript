d98ac45d889347dd4f3adb8adcb7015e2fd12968
Balwinder413 <balwinderghangas99@gmail.com>
2025-07-22 19:03:33 +0530

    Create else if.js

---
+++ condition/else if.js
@@ -0,0 +1,13 @@
+//else if condition
+let mode= "dark";
+let color;
+if(mode=== "dark"){
+color = "Red";
+}
+else if (mode=== "pink"){
+    color="violet";
+}
+else{
+    Color= "white";
+}
+console.log(color);
