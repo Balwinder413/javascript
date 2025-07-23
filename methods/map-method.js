1a24a55f8d1ec9e77ff404b4ec3dfece7ea6691a
Balwinder413 <balwinderghangas99@gmail.com>
2025-07-23 21:35:39 +0530

    Create map-method.js

---
+++ methods/map-method.js
@@ -0,0 +1,12 @@
+
+// map method 
+let  arr=[1,2,3,3,4,4,6]
+arr.map((val)=>{
+     console.log(val);
+})
+
+let array=[3,4,5,6,7,8]
+ let newarr=array.map((val)=>{
+     return val;
+})
+console.log(newarr);
