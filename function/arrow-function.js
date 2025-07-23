5098f31769e19159dacd7c2355fa94c25e8bc40d
Balwinder413 <balwinderghangas99@gmail.com>
2025-07-23 17:16:37 +0530

    Create arrow-function.js

---
+++ function/arrow-function.js
@@ -0,0 +1,19 @@
+// arrow function
+const  arrowSum=(a,b)=>{
+    console.log(a+b)
+}
+arrowSum(2,4)
+
+
+const arrowSub=(a,b)=>{
+    console.log(a-b);
+}
+arrowSub(2,2)
+
+// value return
+let  arrowmulti=(a,b)=>{
+    s= a*b;
+    return s;
+}
+ let value=arrowmulti(2,2)
+ console.log(value);
